ea084340f56c97b8010e74fa09cf9e527ce461a5
Joshua Dinh <joshuahungdinh@gmail.com>
2021-03-22 17:55:26 -0700

    updated styling

--- src/Components/PieChart/PieChart.js
+++ src/Components/PieChart/PieChart.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Pie } from "react-chartjs-2";
+import numeral from "numeral";
 import { connect } from "react-redux";
 import "./pieChart.css";
 
@@ -25,24 +26,33 @@ const PieChart = ({ allCoinData }) => {
     pieChartLabels.push(item.name);
   });
 
-  const data = {
-    maintainAspectRatio: false,
-    responsive: true,
-    labels: pieChartLabels,
-    datasets: [
-      {
-        data: pieChartData,
-        backgroundColor: [
-          "rgba(0, 0, 0, 0.85)",
-          "rgba(0, 0, 0, 0.7)",
-          "rgba(0, 0, 0, 0.6)",
-          "rgba(0, 0, 0, 0.5)",
-          "rgba(0, 0, 0, 0.4)",
-        ],
-        hoverBackgroundColor: " rgba(255, 255, 255, 0.9)",
-      },
-    ],
+  const data = (canvas) => {
+    const ctx = canvas.getContext("2d");
+
+    let gradient = ctx.createLinearGradient(0, 0, 0, 240);
+    gradient.addColorStop(0.7, "rgba(255,255,255, 0.6)");
+    gradient.addColorStop(0.3, "rgba(0,0,0,0.37");
+    gradient.addColorStop(1, "rgba(0,0,0,0.46");
+    return {
+      maintainAspectRatio: false,
+      responsive: true,
+      labels: pieChartLabels,
+      datasets: [
+        {
+          data: pieChartData,
+          backgroundColor: [
+            "rgba(0, 0, 0, 0.8)",
+            "rgba(0, 0, 0, 0.7)",
+            "rgba(0, 0, 0, 0.6)",
+            "rgba(0, 0, 0, 0.5)",
+            "rgba(0, 0, 0, 0.4)",
+          ],
+          hoverBackgroundColor: gradient,
+        },
+      ],
+    };
   };
+
   return (
     <div className="pieChart">
       <Pie data={data} options={options} />
