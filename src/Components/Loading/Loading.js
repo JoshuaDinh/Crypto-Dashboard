ce43c06e35b2c32d009b00816bfb31d2e83d045d
Joshua Dinh <joshuahungdinh@gmail.com>
2021-07-26 22:30:11 -0700

    refactored and extracted loading animation dots into seperate component

---
+++ src/Components/Loading/Loading.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./loading.css";
+
+const Loading = () => {
+  return (
+    <div className="loading">
+      <div className="loading-dot"></div>
+      <div className="loading-dot"></div>
+      <div className="loading-dot"></div>
+    </div>
+  );
+};
+
+export default Loading;
