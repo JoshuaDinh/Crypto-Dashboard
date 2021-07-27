2023a09dcc58c3be00f6ae4ff8731575e011da7c
Joshua Dinh <joshuahungdinh@gmail.com>
2021-07-26 22:33:53 -0700

    refactored ath component with datacards

--- src/Components/AllTimeData/AllTimeData.js
+++ src/Components/AllTimeData/AllTimeData.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./allTimeData.css";
 import { connect } from "react-redux";
 import numeral from "numeral";
+import DataCard from "../DataCard/DataCard";
+import Loading from "../Loading/Loading";
 
 const AllTimeData = ({ selectedCoin }) => {
   // Formats all time date
@@ -11,63 +13,76 @@ const AllTimeData = ({ selectedCoin }) => {
 
   return (
     <div className="allTimeData">
-      <h4 className="allTimeData-header"> All Time Data</h4>
-      <div className="allTime-data-container">
-        <div className="allTimeData-item">
-          <p>
-            <span className="ath">ATH: </span>
-            {/* Ternary determines what to display if no coin is selected/searched */}
-            {selectedCoin.searchedCoin ? (
-              ` $${numeral(selectedCoin.searchedCoin?.ath).format("0,00.00")}`
-            ) : (
-              <span>...</span>
-            )}
-          </p>
-          <p>
-            <span className="ath">ATH Change: </span>
-
-            {selectedCoin.searchedCoin ? (
-              ` % ${numeral(
-                selectedCoin.searchedCoin?.ath_change_percentage
-              ).format("0.00a")}`
-            ) : (
-              <span>...</span>
-            )}
-          </p>
-          <p>
-            <span className="ath">ATH Date : </span>
-            {selectedCoin.searchedCoin?.ath_date
-              ? formatDate(selectedCoin.searchedCoin.ath_date)
-              : "..."}
-          </p>
-        </div>
-        <div className="allTimeData-item">
-          <p>
-            <span className="atl">ATL: </span>
-            {selectedCoin.searchedCoin ? (
-              ` $${numeral(selectedCoin.searchedCoin?.atl).format("0,00.00")}`
-            ) : (
-              <span>...</span>
-            )}
-          </p>
-          <p>
-            <span className="atl"> ATL Change: </span>
-            {selectedCoin.searchedCoin ? (
-              ` % ${numeral(
-                selectedCoin.searchedCoin?.atl_change_percentage
-              ).format("0,0")}`
-            ) : (
-              <span>...</span>
-            )}
-          </p>
-          <p>
-            <span className="atl"> ATL Date : </span>
-            {selectedCoin.searchedCoin?.atl_date
-              ? formatDate(selectedCoin.searchedCoin.atl_date)
-              : "..."}
-          </p>
-        </div>
-      </div>
+      <DataCard
+        title="All Time High"
+        highLow="green"
+        info={
+          selectedCoin.searchedCoin ? (
+            ` $${numeral(selectedCoin.searchedCoin?.ath).format("0,00.00")}`
+          ) : (
+            <Loading />
+          )
+        }
+      />
+      <DataCard
+        title="All Time Low"
+        highLow="red"
+        info={
+          selectedCoin.searchedCoin ? (
+            ` $${numeral(selectedCoin.searchedCoin?.atl).format("0,00.00")}`
+          ) : (
+            <Loading />
+          )
+        }
+      />
+      <DataCard
+        title="ATH Change"
+        highLow="green"
+        info={
+          selectedCoin.searchedCoin ? (
+            ` % ${numeral(
+              selectedCoin.searchedCoin?.ath_change_percentage
+            ).format("0,0")}`
+          ) : (
+            <Loading />
+          )
+        }
+      />
+      <DataCard
+        title="ATL Change"
+        highLow="red"
+        info={
+          selectedCoin.searchedCoin ? (
+            ` % ${numeral(
+              selectedCoin.searchedCoin?.atl_change_percentage
+            ).format("0,0")}`
+          ) : (
+            <Loading />
+          )
+        }
+      />
+      <DataCard
+        title="ATH Date"
+        highLow="green"
+        info={
+          selectedCoin.searchedCoin?.ath_date ? (
+            formatDate(selectedCoin.searchedCoin.ath_date)
+          ) : (
+            <Loading />
+          )
+        }
+      />
+      <DataCard
+        title="ATL Date"
+        highLow="red"
+        info={
+          selectedCoin.searchedCoin?.ath_date ? (
+            formatDate(selectedCoin.searchedCoin.atl_date)
+          ) : (
+            <Loading />
+          )
+        }
+      />
     </div>
   );
 };
@@ -79,3 +94,57 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps)(AllTimeData);
+
+// <div className="allTimeData">
+// <h4 className="allTimeData-header"> All Time Data</h4>
+// <div className="allTime-data-container">
+//   <div className="allTimeData-item">
+//     <p>
+//       <span className="ath">All Time High: </span>
+{
+  /* Ternary determines what to display if no coin is selected/searched */
+}
+//       {selectedCoin.searchedCoin ? (
+//         ` $${numeral(selectedCoin.searchedCoin?.ath).format("0,00.00")}`
+//       ) : (
+//         <span>...</span>
+//       )}
+//     </p>
+//     <p>
+//       <span className="ath">ATH Change: </span>
+
+//
+//     </p>
+//     <p>
+//       <span className="ath">ATH Date : </span>
+//
+//     </p>
+//   </div>
+//   <div className="allTimeData-item">
+//     <p>
+//       <span className="atl">All Time Low: </span>
+//       {selectedCoin.searchedCoin ? (
+//         ` $${numeral(selectedCoin.searchedCoin?.atl).format("0,00.00")}`
+//       ) : (
+//         <span>...</span>
+//       )}
+//     </p>
+//     <p>
+//       <span className="atl"> ATL Change: </span>
+//       {selectedCoin.searchedCoin ? (
+//         ` % ${numeral(
+//           selectedCoin.searchedCoin?.atl_change_percentage
+//         ).format("0,0")}`
+//       ) : (
+//         <span>...</span>
+//       )}
+//     </p>
+//     <p>
+//       <span className="atl"> ATL Date : </span>
+//       {selectedCoin.searchedCoin?.atl_date
+//         ? formatDate(selectedCoin.searchedCoin.atl_date)
+//         : "..."}
+//     </p>
+//   </div>
+// </div>
+// </div>
