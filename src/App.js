8d98222d2e1557a42fe2677319871b4548cf9c81
Joshua Dinh <joshuahungdinh@gmail.com>
2021-08-18 15:02:16 -0700

    updated routing to subdomain

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/crypto-coins">
+        <Route path="/">
           <div className="App">
             <div className="app-left-container">
               <Table />
